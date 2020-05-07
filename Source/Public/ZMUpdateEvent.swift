72e460b08f11e96452dcbdcdfc0d191305479a2f
李晓佳 <353243186@qq.com>
2020-05-07 10:39:28 +0800

    添加itask系统通知类型和动态系统消息通知类型

--- Source/Public/ZMUpdateEvent.swift
+++ Source/Public/ZMUpdateEvent.swift
@@ -92,6 +92,8 @@ import WireUtilities
     case conversationUpdateBlockTime = 110    //群主对成员设置禁言状态
     case conversationAppMessageAdd = 111      //群应用通知
     case userPasswordReset = 112               //密码重置通知
+    case iTaskServiceMessageAdd = 113               //itask公众号通知
+    case iTaskDynamicMessageAdd = 114               //itask消息动态服务通知
     case _LAST  /// ->->->->->!!! Keep this at the end of this enum !!!<-<-<-<-<-
     /// It is used to enumerate values. Hardcoding the values of this enums in tests gets very easily out of sync
 }
@@ -205,6 +207,10 @@ extension ZMUpdateEventType {
             return "user.properties-set"
         case .userPropertiesDelete:
             return "user.properties-delete"
+        case .iTaskServiceMessageAdd:
+            return "itask.user-service-notice"
+        case .iTaskDynamicMessageAdd:
+            return "itask.dynamic-message-notice"
         case ._LAST:
             return nil
         }
